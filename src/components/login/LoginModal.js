d2bd11efa4931d6bbba154c4cd678752b42f505f
James Marshall <jameswhmarshallactions@gmail.com>
2021-09-19 15:24:56 +1000

    building out first component

---
+++ src/components/login/LoginModal.js
@@ -0,0 +1,18 @@
+import React from 'react-dom'
+import logo from '../../assets/images/logo.svg';
+
+function LoginModal() {
+
+
+    return(
+        <div className='LoginModal'>
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>This is the login window</h2>
+
+        </div>
+    )
+
+}
+
+
+export default LoginModal
